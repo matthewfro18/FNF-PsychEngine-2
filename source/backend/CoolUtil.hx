a2be6ed2ae87311bb25f5497dc90a18d22b0b56e
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2024-08-20 17:40:22 -0400

    Update CoolUtil.hx

--- source/backend/CoolUtil.hx
+++ source/backend/CoolUtil.hx
@@ -97,6 +97,19 @@ class CoolUtil
 		return dumbArray;
 	}
 
+	public static function getMinAndMax(value1:Float, value2:Float):Array<Float>
+	{
+		var minAndMaxs = new Array<Float>();
+
+		var min = Math.min(value1, value2);
+		var max = Math.max(value1, value2);
+
+		minAndMaxs.push(min);
+		minAndMaxs.push(max);
+
+		return minAndMaxs;
+	}
+
 	inline public static function browserLoad(site:String) {
 		#if linux
 		Sys.command('/usr/bin/xdg-open', [site]);
