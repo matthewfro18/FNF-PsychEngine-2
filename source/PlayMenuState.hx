c5039c6e0949b609177a0aa8e2d78af0fa7b2afd
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2024-08-26 17:14:22 -0400

    Update PlayMenuState.hx

--- source/PlayMenuState.hx
+++ source/PlayMenuState.hx
@@ -35,6 +35,7 @@ class PlayMenuState extends MusicBeatState
 	var rightOption:String = 'options';
 
 	var magenta:FlxSprite;
+	var bgMenu:FlxBackdrop;
 	var camFollow:FlxObject;
 
 	override function create()
