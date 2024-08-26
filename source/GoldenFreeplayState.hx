eac760e0949838c385025f43c3e95fe445e962c7
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2024-08-26 18:49:40 -0400

    Update GoldenFreeplayState.hx

--- source/GoldenFreeplayState.hx
+++ source/GoldenFreeplayState.hx
@@ -123,16 +123,16 @@ class GoldenFreeplayState extends MusicBeatState
 
         super.update(p);
 
-        if (controls.UP_P)
+        if (Controls.UP_P)
             changeSelection(-1);
 
-        if (controls.DOWN_P)
+        if (Controls.DOWN_P)
             changeSelection(1);
 
-        if (controls.BACK)
+        if (Controls.BACK)
             FlxG.switchState(new MainMenuState());
 
-        if (controls.ACCEPT)
+        if (Controls.ACCEPT)
 		{
             switch (songs[curSelected].songName.toLowerCase()) {
                 case 'unknown':
