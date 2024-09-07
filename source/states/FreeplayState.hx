6e69133c56569229a59b9c44c6d975521aaa8b76
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2024-09-07 07:20:33 -0400

    Update FreeplayState.hx

--- source/states/FreeplayState.hx
+++ source/states/FreeplayState.hx
@@ -72,6 +72,42 @@ class FreeplayState extends MusicBeatState
 					function() MusicBeatState.switchState(new states.MainMenuState())));
 				return;
 			}
+		if(WeekData.weeksList.length < 1 && PlayState.isUniverse)
+			{
+				FlxTransitionableState.skipNextTransIn = true;
+				persistentUpdate = false;
+				MusicBeatState.switchState(new states.ErrorState("NO WEEKS ADDED FOR FREEPLAY\n\nPress ACCEPT to go to the Week Editor Menu.\nPress BACK to return to Main Menu.",
+					function() MusicBeatState.switchState(new states.editors.WeekEditorState()),
+					function() MusicBeatState.switchState(new states.MainMenuState())));
+				return;
+			}
+		if(WeekData.weeksList.length < 1 && PlayState.isGolden)
+			{
+				FlxTransitionableState.skipNextTransIn = true;
+				persistentUpdate = false;
+				MusicBeatState.switchState(new states.ErrorState("NO WEEKS ADDED FOR FREEPLAY\n\nPress ACCEPT to go to the Week Editor Menu.\nPress BACK to return to Main Menu.",
+					function() MusicBeatState.switchState(new states.editors.WeekEditorState()),
+					function() MusicBeatState.switchState(new states.MainMenuState())));
+				return;
+			}
+		if(WeekData.weeksList.length < 1 && PlayState.isDaveAndBambi)
+			{
+				FlxTransitionableState.skipNextTransIn = true;
+				persistentUpdate = false;
+				MusicBeatState.switchState(new states.ErrorState("NO WEEKS ADDED FOR FREEPLAY\n\nPress ACCEPT to go to the Week Editor Menu.\nPress BACK to return to Main Menu.",
+					function() MusicBeatState.switchState(new states.editors.WeekEditorState()),
+					function() MusicBeatState.switchState(new states.MainMenuState())));
+				return;
+			}
+		if(WeekData.weeksList.length < 1 && PlayState.isSecret)
+			{
+				FlxTransitionableState.skipNextTransIn = true;
+				persistentUpdate = false;
+				MusicBeatState.switchState(new states.ErrorState("NO WEEKS ADDED FOR FREEPLAY\n\nPress ACCEPT to go to the Week Editor Menu.\nPress BACK to return to Main Menu.",
+					function() MusicBeatState.switchState(new states.editors.WeekEditorState()),
+					function() MusicBeatState.switchState(new states.MainMenuState())));
+				return;
+			}
 
 		for (i in 0...WeekData.weeksList.length)
 		{
