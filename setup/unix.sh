5bdf60694dcbf0ba067263c31fe345ae4ed96853
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2024-09-07 07:33:15 -0400

    Update unix.sh

--- setup/unix.sh
+++ setup/unix.sh
@@ -9,7 +9,7 @@ haxelib install lime 8.0.0
 haxelib install openfl 9.3.3
 haxelib install flixel 5.6.1
 haxelib install flixel 4.11.0
-haxelib install flixel-addons 2.9.0
+haxelib install flixel-addons 3.2.2
 haxelib install flixel-tools 1.5.1
 haxelib install hscript-iris 1.0.2
 haxelib install tjson 1.4.0
